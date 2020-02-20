a27a4dc7010fab77d637eedeaaaca8619facdcf7
Preston Jenkins <prestonjenkins@Prestons-MacBook-Pro.local>
2020-02-20 11:26:45 -0700

    auth ctrl is finished.

--- server/authController.js
+++ server/authController.js
@@ -1,16 +1,56 @@
 const bcrypt = require('bcryptjs');
 
 module.exports = {
-    login: () => {
-        
+    login: async (req, res, next) => {
+        const { email, password } = req.body
+        const { session } = req
+        const db = req.app.get('db')
+
+        let user = await db.check_user([email])
+        user = user[0]
+        if (!user) {
+            return res.status(400).send('Email not found.')
+        }
+
+        const authenticated = bcrypt.compareSync(password, user.user_password)
+
+        if (authenticated) {
+            delete user.user_password
+            session.user = user
+            res.status(202).send(session.user)
+        } else {
+            res.status(401).send('Incorrect password.')
+        }
+
     },
-    register: () => {
-        
+    register: async (req, res, next) => {
+        const { email, password } = req.body
+        const { session } = req
+        const db = req.app.get('db')
+
+        let user = await db.check_user([email])
+        user = user[0]
+        if (user) {
+            return res.status(400).send('User already exists.')
+        }
+
+        const salt = bcrypt.genSaltSync(20)
+        const hash = bcrypt.hashSync(password, salt)
+
+        let newUser = await db.register_user({email, hash})
+        newUser = newUser[0]
+        session.user = newUser
+        res.status(201).send(session.user)
     },
-    logout: () => {
-        
+    logout: (req, res) => {
+        req.session.destroy()
+        res.sendStatus(200)
     },
-    getUser: () => {
-        
+    getUser: (req, res) => {
+        if (req.session.user) {
+            res.status(200).send(req.session.user)
+        } else {
+            res.sendStatus(200)
+        }
     }
 };
